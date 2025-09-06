1eb4c70fa1587a80b1a26c7dac6cee5cca7de084
Erik Sikich <erik.sikich@gmail.com>
2025-09-05 22:11:33 -0500

    Update ecs.js

--- ecs.js
+++ ecs.js
@@ -1,99 +1,118 @@
 /** =========================
- *  Entity Component System - Using Game Namespace
+ *  Entity Component System - Complete Final Version
  *  ========================= */
 
-/**
- * Create a component type if it doesn't exist
- */
-function createComponent(type) {
-    if (!Game.ecs.components[type]) {
-        Game.ecs.components[type] = {};
-    }
-}
-
-/**
- * Create a new entity and return its ID
- */
-function createEntity() {
-    const id = Game.ecs.nextEntityId++;
-    Game.ecs.entities.add(id);
-    return id;
-}
-
-/**
- * Add a component to an entity
- */
-function addComponent(eid, type, data) {
-    createComponent(type);
-    Game.ecs.components[type][eid] = data;
-}
-
-/**
- * Get a component from an entity
- */
-function getComponent(eid, type) {
-    return (Game.ecs.components[type] && Game.ecs.components[type][eid]) || null;
-}
-
-/**
- * Check if an entity has a component
- */
-function hasComponent(eid, type) {
-    return !!getComponent(eid, type);
-}
-
-/**
- * Get all entities that have all of the specified component types
- */
-function getEntitiesWith(types) {
-    const out = [];
-    Game.ecs.entities.forEach(function(eid) {
-        for (let i = 0; i < types.length; i++) {
-            if (!hasComponent(eid, types[i])) {
-                return; // Skip this entity
+// ECS Module with private state and public API
+Game.ECS = (function() {
+    'use strict';
+    
+    // Private data (only accessible within this module)
+    let nextEntityId = 1;
+    let entities = new Set();
+    let components = {};
+    let eventQueue = [];
+    
+    // Public API
+    return {
+        // Entity management
+        createEntity() {
+            const id = nextEntityId++;
+            entities.add(id);
+            return id;
+        },
+        
+        destroyEntity(eid) {
+            entities.delete(eid);
+            for (const componentType in components) {
+                if (components[componentType] && 
+                    components[componentType][eid] !== undefined) {
+                    delete components[componentType][eid];
+                }
             }
+        },
+        
+        // Component management
+        addComponent(eid, type, data) {
+            this.createComponentType(type);
+            components[type][eid] = data;
+        },
+        
+        getComponent(eid, type) {
+            return (components[type] && components[type][eid]) || null;
+        },
+        
+        hasComponent(eid, type) {
+            return !!this.getComponent(eid, type);
+        },
+        
+        createComponentType(type) {
+            if (!components[type]) {
+                components[type] = {};
+            }
+        },
+        
+        // Query system
+        getEntitiesWith(types) {
+            const out = [];
+            entities.forEach(eid => {
+                if (types.every(type => this.hasComponent(eid, type))) {
+                    out.push(eid);
+                }
+            });
+            return out;
+        },
+        
+        getEntitiesAt(x, y) {
+            const out = [];
+            const list = this.getEntitiesWith(['position']);
+            for (let i = 0; i < list.length; i++) {
+                const p = this.getComponent(list[i], 'position');
+                if (p && p.x === x && p.y === y) out.push(list[i]);
+            }
+            return out;
+        },
+        
+        // Event system
+        postEvent(event) {
+            eventQueue.push(event);
+        },
+        
+        drainEvents() {
+            const events = eventQueue.slice();
+            eventQueue = [];
+            return events;
+        },
+        
+        // System management
+        reset() {
+            nextEntityId = 1;
+            entities.clear();
+            components = {};
+            eventQueue = [];
+        },
+        
+        // Debug/utility methods
+        getEntityCount() {
+            return entities.size;
+        },
+        
+        getComponentTypes() {
+            return Object.keys(components);
+        },
+        
+        getAllEntities() {
+            return Array.from(entities);
         }
-        out.push(eid);
-    });
-    return out;
-}
+    };
+})();
 
-/**
- * Remove an entity and all its components
- */
-function destroyEntity(eid) {
-    Game.ecs.entities.delete(eid);
-    for (const componentType in Game.ecs.components) {
-        if (Game.ecs.components[componentType] && 
-            Game.ecs.components[componentType][eid] !== undefined) {
-            delete Game.ecs.components[componentType][eid];
-        }
-    }
-}
-
-/**
- * Add an event to the event queue
- */
-function postEvent(event) {
-    Game.ecs.eventQueue.push(event);
-}
-
-/**
- * Get all queued events and clear the queue
- */
-function drainEvents() {
-    const events = Game.ecs.eventQueue.slice();
-    Game.ecs.eventQueue = [];
-    return events;
-}
-
-// Alternative: More object-oriented approach for ECS functions
-// You could also organize the functions like this:
-Game.ecs.createEntity = createEntity;
-Game.ecs.addComponent = addComponent;
-Game.ecs.getComponent = getComponent;
-Game.ecs.hasComponent = hasComponent;
-Game.ecs.getEntitiesWith = getEntitiesWith;
-Game.ecs.destroyEntity = destroyEntity;
-Game.ecs.postEvent = postEvent;
-Game.ecs.drainEvents = drainEvents;
+// Backward compatibility functions
+function createEntity() { return Game.ECS.createEntity(); }
+function destroyEntity(eid) { return Game.ECS.destroyEntity(eid); }
+function addComponent(eid, type, data) { return Game.ECS.addComponent(eid, type, data); }
+function getComponent(eid, type) { return Game.ECS.getComponent(eid, type); }
+function hasComponent(eid, type) { return Game.ECS.hasComponent(eid, type); }
+function getEntitiesWith(types) { return Game.ECS.getEntitiesWith(types); }
+function getEntitiesAt(x, y) { return Game.ECS.getEntitiesAt(x, y); }
+function postEvent(event) { return Game.ECS.postEvent(event); }
+function drainEvents() { return Game.ECS.drainEvents(); }
